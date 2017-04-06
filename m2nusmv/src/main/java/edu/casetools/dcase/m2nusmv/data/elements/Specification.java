a3deab78f985e74fbf66ade38090068096f6aff7
Unai Alegre <u.alegre@mdx.ac.uk>
2017-04-06 18:09:51 +0100

    Added specifications

--- m2nusmv/src/main/java/edu/casetools/dcase/m2nusmv/data/elements/Specification.java
+++ m2nusmv/src/main/java/edu/casetools/dcase/m2nusmv/data/elements/Specification.java
@@ -17,25 +17,37 @@
 package edu.casetools.dcase.m2nusmv.data.elements;
 
 public class Specification {
-    
+
+    public enum TYPE {
+	CTL, LTL, PSL, INVARIANT, COMPUTE
+    }
+
     private String id;
     private String spec;
-    
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getSpec() {
-		return spec;
-	}
-	
-	public void setSpec(String spec) {
-		this.spec = spec;
-	}
-    
-    
+    private TYPE type;
+
+    public String getId() {
+	return id;
+    }
+
+    public void setId(String id) {
+	this.id = id;
+    }
+
+    public String getSpec() {
+	return spec;
+    }
+
+    public void setSpec(String spec) {
+	this.spec = spec;
+    }
+
+    public TYPE getType() {
+	return type;
+    }
+
+    public void setType(TYPE type) {
+	this.type = type;
+    }
+
 }
